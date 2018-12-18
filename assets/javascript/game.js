783f0ee49f085dfaf11c5c9e039a260c6342fce3
Estevan Cruz <Estevancruz@Estevans-MBP.lan>
2018-12-18 11:18:18 -0600

    psuedo code outline on js

---
+++ assets/javascript/game.js
@@ -0,0 +1,11 @@
+// Random number has to be generated //
+
+// Wins and Losses have to be recorded //
+
+// If won/loss random number has to restart //
+
+// Gems need to be assign values and be click-able buttons //
+
+// Gem values must appear on Your Total Score //
+
+// Gem values must be reassigned after every round //
